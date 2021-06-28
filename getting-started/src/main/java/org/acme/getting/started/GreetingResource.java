ade8bfa413362804e67e6cfbd07168b055d11564
fpaoluzi <fpaoluzi@sogei.it>
2021-06-28 14:22:51 +0000

    commit test

--- getting-started/src/main/java/org/acme/getting/started/GreetingResource.java
+++ getting-started/src/main/java/org/acme/getting/started/GreetingResource.java
@@ -7,7 +7,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
-
+//new comment
 @Path("/hello")
 public class GreetingResource {
 
